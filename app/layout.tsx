37918d2d68fd364ab82ffade902cd94a25ae8e16
Adham <adham00109@gmai.com>
2023-08-09 20:45:23 +0800

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,11 +7,17 @@ import Footer from "@/components/Footer"
 export const metadata = {
     title: "Giggers",
     description: "A Gig Worker Portal",
+    icons: {
+        icon: "web-icon.png",
+    }
 }
 
 export default function RootLayout({ children, } : { children: React.ReactNode }) {
     return (
         <html lang="en">
+            <head>
+                <link rel="icon" href="web-icon.png"/>
+            </head>
             <body>
                 {/* we can notice how the app is divided by 3 section -> Navbar, main, and Footer */}
                 {/* the Navbar and the Footer will always be available through the whole app */}
@@ -25,3 +31,4 @@ export default function RootLayout({ children, } : { children: React.ReactNode }
         </html>
     )
 }
+
