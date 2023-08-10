e7698de5cd0848217ae57b1f78cd8bf02379f56e
Adham <adham00109@gmai.com>
2023-08-10 11:19:15 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,8 @@ import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions";
 
 type SearchParams = {
-    category?: string | undefined | null,
-    endCursor?: string| undefined | null,
+    category?: string,
+    endCursor?: string,
 }
 
 type Props = {
